cb375861b6dba528b9be57eb08639ea95e5bcc8c
yaochenghao <yaochenghao@bytedance.com>
2018-03-15 21:01:20 +0800

    add: basic text render

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,8 @@ new typesetting({
     text: '接通率=（未回应+已回应的线索量）÷（未接通+未回应+已回应的线索量)',
     width: '92px',
     fontSize: '14px',
+    lineHeight: '20px',
+    color: 'black',
     fontFamily: 'mocrosoft yahei',
+    padding: '0px',
 });
--- src/typesetting.ts
+++ src/typesetting.ts
@@ -1,8 +1,13 @@
+import { parse } from 'babylon';
+
 interface textConfig {
     text: string;
     width: string;
     fontSize: string;
     fontFamily: string;
+    lineHeight?: string | number;
+    color: string;
+    padding: string;
 }
 export default class TypeSetting {
     config: textConfig;
@@ -14,8 +19,9 @@ export default class TypeSetting {
         this.config = config;
         this.body = document.body;
 
-        this.testCaseInit();
+        this.canvasInit();
         this.init();
+        this.testCaseInit();
     }
 
     testCaseInit() {
@@ -24,13 +30,71 @@ export default class TypeSetting {
         div.style.width = this.config.width;
         div.style.fontSize = this.config.fontSize;
         div.style.fontFamily = this.config.fontFamily;
+        div.style.padding = this.config.padding;
+        div.style.color = this.config.color;
         div.style.border = '1px solid black';
 
+        this.canvas.style.border = '1px solid black';
+        this.canvas.style.margin = '20px 0 0 0';
+
         this.body.appendChild(div);
+        this.body.appendChild(this.canvas);
+    }
+
+    canvasInit() {
+        this.canvas = document.createElement('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.canvas.width = parseInt(this.config.width, 10);
+        this.canvas.height = this.init();
     }
 
     init() {
-        this.canvas = document.createElement('canvas');
-        this.ctx = this.canvas.getContext('2d');
+        // 暂定4边padding等宽
+        const startX = parseInt(this.config.padding, 10);
+        const startY = startX;
+        const endX = parseInt(this.config.width, 10) - startX;
+        const getWidth = (val: string) => {
+            this.ctx.save();
+            this.ctx.font = `${this.config.fontSize} ${this.config.fontFamily}`;
+            const height = this.ctx.measureText(val).width;
+            this.ctx.restore();
+            return height;
+        };
+        const getLineHeight = () => {
+            const height = getWidth('w');
+            if (!this.config.lineHeight) {
+                // 不存在
+                return 1.5 * height;
+            }
+            if (typeof this.config.lineHeight === 'number') {
+                return this.config.lineHeight * height;
+            } else {
+                return parseInt(this.config.lineHeight, 10);
+            }
+        };
+        const lineHeight = getLineHeight();
+
+        let beginX = startX;
+        let beginY = startY + lineHeight;
+        this.ctx.save();
+        this.ctx.fillStyle = this.config.color;
+        this.ctx.font = `${this.config.fontSize} ${this.config.fontFamily}`;
+        for (let i of this.config.text) {
+            const width = getWidth(i);
+            const eX = beginX + width;
+            if (eX > endX) {
+                beginX = startX;
+                beginY += lineHeight;
+                this.ctx.fillText(i, beginX, beginY);
+                beginX += width;
+            } else {
+                console.log(beginX);
+                this.ctx.fillText(i, beginX, beginY);
+                beginX = eX;
+            }
+        }
+        this.ctx.restore();
+
+        return beginY + startY + 2;
     }
 }
